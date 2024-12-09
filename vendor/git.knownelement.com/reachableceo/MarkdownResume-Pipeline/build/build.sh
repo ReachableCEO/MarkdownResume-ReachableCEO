401e997f092e13012605b891b51a07f4cd877bba
ReachableCEO <reachableceo@turnsys.com>
2024-12-09 15:46:41 -0600

    and that's a 1.0 wrap. whew.

--- vendor/git.knownelement.com/reachableceo/MarkdownResume-Pipeline/build/build.sh
+++ vendor/git.knownelement.com/reachableceo/MarkdownResume-Pipeline/build/build.sh
@@ -25,7 +25,7 @@ export PAGEBACKGROUND="../vendor/git.knownelement.com/ExternalVendorCode/pandoc-
 ############################################################
 
 readonly MO_PATH="bash ../vendor/git.knownelement.com/ExternalVendorCode/mo/mo"
-readonly BUILD_OUTPUT_DIR="D:\tsys\@ReachableCEO\resume.reachableceo.com"
+readonly BUILD_OUTPUT_DIR="D:/tsys/@ReachableCEO/resume.reachableceo.com"
 readonly BUILD_TEMP_DIR="../build-temp"
 readonly BUILDYAML_JOBBOARD="$BUILD_TEMP_DIR/JobBoard.yml"
 readonly BUILDYAML_CLIENTSUBMISSION="$BUILD_TEMP_DIR/ClientSubmission.yml"
@@ -37,18 +37,14 @@ JobBoardPDFOutputFile="$BUILD_OUTPUT_DIR/job-board/CharlesNWyble-Resume.pdf"
 JobBoardMSWordOutputFile="$BUILD_OUTPUT_DIR/job-board/CharlesNWyble-Resume.doc"
 
 ClientSubmissionMarkdownOutputFile="$BUILD_TEMP_DIR/client-submission/Resume.md"
-ClientSubmissionPDFOutputFile="$BUILD_OUTPUT_DIR/client-submission//Resume.pdf"
-ClientSubmissionMSWordOutputFile="$BUILD_OUTPUT_DIR/client-submission/Resume.doc"
+ClientSubmissionPDFOutputFile="$BUILD_OUTPUT_DIR/client-submit//CharlesNWyble-Resume.pdf"
+ClientSubmissionMSWordOutputFile="$BUILD_OUTPUT_DIR/client-submit/CharlesNWyble-Resume.doc"
 
 rm $BUILDYAML_JOBBOARD
 rm $JobBoardMarkdownOutputFile
-rm $JobBoardPDFOutputFile
-rm $JobBoardMSWordOutputFile
 
 rm $BUILDYAML_CLIENTSUBMISSION
 rm $ClientSubmissionMarkdownOutputFile
-rm $ClientSubmissionPDFOutputFile
-rm $ClientSubmissionMSWordOutputFile
 
 # Expand variables into rendered YAML files. These will be used by pandoc to create the output artifacts
 
